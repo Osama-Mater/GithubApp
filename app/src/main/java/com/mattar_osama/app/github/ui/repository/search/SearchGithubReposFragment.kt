5de32324acce7afa32bc5765d93b0ca0aa1c4200
Osama Ahmed Mattar <osama01145@fci.zu.edu.eg>
2022-02-08 15:06:19 +0200

    Migrate from Kotlin synthetics to Jetpack view binding

--- app/src/main/java/com/mattar_osama/app/github/ui/repository/search/SearchGithubReposFragment.kt
+++ app/src/main/java/com/mattar_osama/app/github/ui/repository/search/SearchGithubReposFragment.kt
@@ -1,9 +1,7 @@
 package com.mattar_osama.app.github.ui.repository.search
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.View
+import android.view.*
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -13,6 +11,7 @@ import com.bumptech.glide.Glide
 import com.mattar_osama.app.github.R
 import com.mattar_osama.app.github.api.NetworkState
 import com.mattar_osama.app.github.base.BaseFragment
+import com.mattar_osama.app.github.databinding.FragmentSearchGithubReposBinding
 import com.mattar_osama.app.github.extensions.onQueryTextChange
 import com.mattar_osama.app.github.ui.repository.search.views.SearchGithubReposAdapter
 import com.mattar_osama.app.github.utils.convertFilterToIndex
@@ -29,12 +28,26 @@ class SearchGithubReposFragment : BaseFragment(), SearchGithubReposAdapter.OnCli
     private val viewModel: SearchGithubReposViewModel by viewModel()
     private lateinit var adapter: SearchGithubReposAdapter
 
+    private var _binding: FragmentSearchGithubReposBinding? = null
+    private val binding get() = _binding!!
+
     // OVERRIDE ---
     override fun getLayoutId(): Int = R.layout.fragment_search_github_repos
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchGithubReposBinding.inflate(
+            inflater, container, false
+        )
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -60,8 +73,8 @@ class SearchGithubReposFragment : BaseFragment(), SearchGithubReposAdapter.OnCli
     }
 
     private fun configureOnClick() {
-        fragment_search_github_repos_empty_list_button.setOnClickListener { viewModel.refreshAllList() }
-        fragment_search_github_repos_fab.setOnClickListener { showDialogWithFilterItems { viewModel.refreshAllList() } }
+        binding.fragmentSearchGithubReposEmptyListButton.setOnClickListener { viewModel.refreshAllList() }
+        binding.fragmentSearchGithubReposFab.setOnClickListener { showDialogWithFilterItems { viewModel.refreshAllList() } }
     }
 
     // CONFIGURATION ---
@@ -81,50 +94,52 @@ class SearchGithubReposFragment : BaseFragment(), SearchGithubReposAdapter.OnCli
 
     private fun configureRecyclerView() {
         adapter = SearchGithubReposAdapter(this)
-        fragment_search_github_repos_rv.adapter = adapter
+        binding.fragmentSearchGithubReposRv.adapter = adapter
     }
 
     private fun configureObservables() {
-        viewModel.networkState?.observe(this, Observer { adapter.updateNetworkState(it) })
-        viewModel.githubRepos.observe(this, Observer { adapter.submitList(it) })
+        viewModel.networkState?.observe(
+            viewLifecycleOwner,
+            Observer { adapter.updateNetworkState(it) })
+        viewModel.githubRepos.observe(viewLifecycleOwner, Observer { adapter.submitList(it) })
     }
 
     // UPDATE UI ----
     private fun updateUIWhenEmptyList(size: Int, networkState: NetworkState?) {
-        fragment_search_github_repos_empty_list_image.visibility = View.GONE
-        fragment_search_github_repos_empty_list_button.visibility = View.GONE
-        fragment_search_github_repos_empty_list_title.visibility = View.GONE
+        binding.fragmentSearchGithubReposEmptyListImage.visibility = View.GONE
+        binding.fragmentSearchGithubReposEmptyListButton.visibility = View.GONE
+        binding.fragmentSearchGithubReposEmptyListTitle.visibility = View.GONE
         if (size == 0) {
             when (networkState) {
                 NetworkState.SUCCESS -> {
                     Glide.with(this).load(R.drawable.ic_directions_run_black_24dp)
-                        .into(fragment_search_github_repos_empty_list_image)
-                    fragment_search_github_repos_empty_list_title.text =
+                        .into(binding.fragmentSearchGithubReposEmptyListImage)
+                    binding.fragmentSearchGithubReposEmptyListTitle.text =
                         getString(R.string.no_result_found)
-                    fragment_search_github_repos_empty_list_image.visibility = View.VISIBLE
-                    fragment_search_github_repos_empty_list_title.visibility = View.VISIBLE
-                    fragment_search_github_repos_empty_list_button.visibility = View.GONE
+                    binding.fragmentSearchGithubReposEmptyListImage.visibility = View.VISIBLE
+                    binding.fragmentSearchGithubReposEmptyListTitle.visibility = View.VISIBLE
+                    binding.fragmentSearchGithubReposEmptyListButton.visibility = View.GONE
                 }
                 NetworkState.FAILED -> {
                     Glide.with(this).load(R.drawable.ic_healing_black_24dp)
-                        .into(fragment_search_github_repos_empty_list_image)
-                    fragment_search_github_repos_empty_list_title.text =
+                        .into(binding.fragmentSearchGithubReposEmptyListImage)
+                    binding.fragmentSearchGithubReposEmptyListTitle.text =
                         getString(R.string.technical_error)
-                    fragment_search_github_repos_empty_list_image.visibility = View.VISIBLE
-                    fragment_search_github_repos_empty_list_title.visibility = View.VISIBLE
-                    fragment_search_github_repos_empty_list_button.visibility = View.VISIBLE
+                    binding.fragmentSearchGithubReposEmptyListImage.visibility = View.VISIBLE
+                    binding.fragmentSearchGithubReposEmptyListTitle.visibility = View.VISIBLE
+                    binding.fragmentSearchGithubReposEmptyListButton.visibility = View.VISIBLE
                 }
             }
         }
     }
 
     private fun updateUIWhenLoading(size: Int, networkState: NetworkState?) {
-        fragment_search_github_repos_progress.visibility =
+        binding.fragmentSearchGithubReposProgress.visibility =
             if (size == 0 && networkState == NetworkState.RUNNING) View.VISIBLE else View.GONE
     }
 
     private fun showDialogWithFilterItems(callback: () -> Unit) {
-        MaterialDialog(this.activity!!).show {
+        MaterialDialog(this.requireActivity()).show {
             title(R.string.filter_popup_title)
             listItemsSingleChoice(
                 R.array.filters,
@@ -136,5 +151,10 @@ class SearchGithubReposFragment : BaseFragment(), SearchGithubReposAdapter.OnCli
                 callback()
             }
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
