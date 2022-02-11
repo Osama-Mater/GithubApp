a1e3015663c248c323a358e4fa85314ccf2905cf
Osama Ahmed Mattar <osama01145@fci.zu.edu.eg>
2022-02-11 20:56:53 +0200

    Use github basic Authorization

--- app/src/main/java/com/mattar_osama/app/github/nertwork/RequestInterceptor.kt
+++ app/src/main/java/com/mattar_osama/app/github/nertwork/RequestInterceptor.kt
@@ -1,21 +1,21 @@
 package com.mattar_osama.app.github.nertwork
 
 import com.mattar_osama.app.github.BuildConfig
+import okhttp3.Credentials
 import okhttp3.Interceptor
 import okhttp3.Response
 
 internal class RequestInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
-        val originalUrl = originalRequest.url
-        val url = originalUrl.newBuilder()
-            .addQueryParameter("client_id", BuildConfig.GithubClientId)
-            .addQueryParameter("client_secret", BuildConfig.GITHUB_AUTHORIZATION_API_KEY)
-            .build()
 
-        val requestBuilder = originalRequest.newBuilder().url(url)
+        val requestBuilder = originalRequest.newBuilder()
         val request = requestBuilder
             .addHeader("Accept", "application/vnd.github.v3+json")
+            .addHeader(
+                "Authorization",
+                Credentials.basic(BuildConfig.GITHUB_USER_NAME, BuildConfig.GITHUB_TOKEN)
+            )
             .build()
         return chain.proceed(request)
     }
